df81d7bc9aed0b933257e2233aab190499809181
williamtansil <williamtansil.public@gmail.com>
2016-09-27 23:26:56 +0700

    Summitted Latihan_1_1

---
+++ scrollbar/scrollbar/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace scrollbar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.test = new System.Windows.Forms.Label();
+            this.max = new System.Windows.Forms.Label();
+            this.min = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.calender = new System.Windows.Forms.DateTimePicker();
+            this.vscrollbar2 = new System.Windows.Forms.VScrollBar();
+            this.vscrollbar1 = new System.Windows.Forms.VScrollBar();
+            this.dll = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menusToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.dll.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.dll);
+            this.tabControl1.ImageList = this.imageList1;
+            this.tabControl1.Location = new System.Drawing.Point(12, 36);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(260, 237);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.test);
+            this.tabPage1.Controls.Add(this.max);
+            this.tabPage1.Controls.Add(this.min);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.calender);
+            this.tabPage1.Controls.Add(this.vscrollbar2);
+            this.tabPage1.Controls.Add(this.vscrollbar1);
+            this.tabPage1.ImageKey = "scrollicon.png";
+            this.tabPage1.Location = new System.Drawing.Point(4, 23);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(252, 210);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "scrollbar";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // test
+            // 
+            this.test.AutoSize = true;
+            this.test.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.test.Location = new System.Drawing.Point(92, 154);
+            this.test.Name = "test";
+            this.test.Size = new System.Drawing.Size(17, 24);
+            this.test.TabIndex = 2;
+            this.test.Text = "-";
+            // 
+            // max
+            // 
+            this.max.AutoSize = true;
+            this.max.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.max.Location = new System.Drawing.Point(92, 61);
+            this.max.Name = "max";
+            this.max.Size = new System.Drawing.Size(17, 24);
+            this.max.TabIndex = 2;
+            this.max.Text = "-";
+            // 
+            // min
+            // 
+            this.min.AutoSize = true;
+            this.min.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.min.Location = new System.Drawing.Point(92, 30);
+            this.min.Name = "min";
+            this.min.Size = new System.Drawing.Size(17, 24);
+            this.min.TabIndex = 2;
+            this.min.Text = "-";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(22, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "MAX";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(28, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "MIN";
+            // 
+            // calender
+            // 
+            this.calender.Location = new System.Drawing.Point(28, 111);
+            this.calender.Name = "calender";
+            this.calender.Size = new System.Drawing.Size(196, 20);
+            this.calender.TabIndex = 1;
+            // 
+            // vscrollbar2
+            // 
+            this.vscrollbar2.LargeChange = 1;
+            this.vscrollbar2.Location = new System.Drawing.Point(220, 15);
+            this.vscrollbar2.Maximum = 10;
+            this.vscrollbar2.Minimum = 1;
+            this.vscrollbar2.Name = "vscrollbar2";
+            this.vscrollbar2.Size = new System.Drawing.Size(17, 80);
+            this.vscrollbar2.TabIndex = 0;
+            this.vscrollbar2.Value = 2;
+            this.vscrollbar2.Scroll += new System.Windows.Forms.ScrollEventHandler(this.scrollbar1);
+            // 
+            // vscrollbar1
+            // 
+            this.vscrollbar1.LargeChange = 1;
+            this.vscrollbar1.Location = new System.Drawing.Point(179, 15);
+            this.vscrollbar1.Maximum = 10;
+            this.vscrollbar1.Minimum = 1;
+            this.vscrollbar1.Name = "vscrollbar1";
+            this.vscrollbar1.Size = new System.Drawing.Size(17, 80);
+            this.vscrollbar1.TabIndex = 0;
+            this.vscrollbar1.Value = 8;
+            this.vscrollbar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.scrollbar1);
+            // 
+            // dll
+            // 
+            this.dll.Controls.Add(this.label1);
+            this.dll.Location = new System.Drawing.Point(4, 23);
+            this.dll.Name = "dll";
+            this.dll.Padding = new System.Windows.Forms.Padding(3);
+            this.dll.Size = new System.Drawing.Size(252, 210);
+            this.dll.TabIndex = 1;
+            this.dll.Text = "dll";
+            this.dll.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(45, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bersambung..";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "scrollicon.png");
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menusToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menusToolStripMenuItem
+            // 
+            this.menusToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.menusToolStripMenuItem.Name = "menusToolStripMenuItem";
+            this.menusToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menusToolStripMenuItem.Text = "Menu";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 291);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.dll.ResumeLayout(false);
+            this.dll.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DateTimePicker calender;
+        private System.Windows.Forms.VScrollBar vscrollbar2;
+        private System.Windows.Forms.VScrollBar vscrollbar1;
+        private System.Windows.Forms.TabPage dll;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menusToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.Label max;
+        private System.Windows.Forms.Label min;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label test;
+    }
+}
+
---
+++ scrollbar/scrollbar/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace scrollbar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void scrollbar1(object sender, ScrollEventArgs e)
+        {
+            if (vscrollbar1.Value < vscrollbar2.Value)
+            {
+                min.Text = vscrollbar1.Value.ToString();
+                max.Text = vscrollbar2.Value.ToString();
+            }
+            else 
+            {
+                min.Text = vscrollbar2.Value.ToString();
+                max.Text = vscrollbar1.Value.ToString();
+            }
+
+            DateTime _temp = new DateTime();
+            _temp = new DateTime(DateTime.Today.Year + Math.Abs(vscrollbar1.Value - vscrollbar2.Value), DateTime.Today.Month, DateTime.Today.Day);
+            calender.MinDate = DateTime.Today.Date;
+            calender.MaxDate = _temp;
+
+            test.Text = Convert.ToString(DateTime.Today.Year + Math.Abs(vscrollbar1.Value - vscrollbar2.Value));
+        }
+    }
+}
